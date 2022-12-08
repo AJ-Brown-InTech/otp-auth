6e7a26577e6e12d2173a356432f4d43c4d8d3410
AJ-Brown-InTech <ajalantbrown@gmail.com>
2022-12-08 17:06:41 -0600

    makefile to create a self signed cert

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+/*
+    1. input a phone number
+    2. check is number exist
+    3. if number exit send a random 6 digit code
+    4. if user input equals 6 digit code authorize
+    5. else throw an error/try again 
+*/
 const express = require('express')
 const app = express()
+const http = require('https')
 
 
