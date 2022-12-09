a365168fb28e9453debc5727f0f4e72842fea49e
AJ-Brown-InTech <ajalantbrown@gmail.com>
2022-12-09 14:02:14 -0600

    sends emails for not, will need to go with a paid service for sms and we aint doing that rn

--- index.js
+++ index.js
@@ -20,15 +20,16 @@ const port = process.env.PORT || 8282
 let emailDate = new Date().getTime();
 let configs = {
     auth: {
-    	username: process.env.USERNAME,
     	user: process.env.EMAIL,
     	pass: process.env.EMAILPASS,
 		},
-    host: 'smtp-mail.outlook.com',
-    service: 'outlook',
-		port: 587,
-		secure: true,
-        to: 'ajalantbrown@gmail.com'
+        service: "Outlook365",
+        host: 'smtp-mail.outlook.com',
+		port: process.env.SMTPPORT,
+		secureConnection: false, //TLS require a secure connection to be false
+    tls:{
+        ciphers:'SSLv3'
+    }
     } 
 
 //transport emails to phone numbers
@@ -38,7 +39,7 @@ const transport = async () =>{
     
     let mailOptions = {
      from: process.env.EMAIL, //sender email
-     to: configs.to, //sender
+     to: 'ajalantbrown@gmail.com', //sender
      subject: 'Premier One-Time Verification Code',
      text: `Your verification code is 123.\nDo not share this code with anyone.`,
     }
@@ -46,36 +47,36 @@ const transport = async () =>{
     let info = await transporter.sendMail(mailOptions)
     console.log('Message sent: %s', info.messageId);
 }
+transport().catch("pigs",console.error);
 
-//emial listener
-var mailListener = new MailListener({
-    username: configs.username,
-    password: configs.pass,
-    host: 'outlook.office365.com',
-    port: 993,
-    tls: false,
-    
-    connTimeout: 10000,
-    authTimeout: 5000,
-    debug: console.log,
-    tlsOptions: { rejectUnauthorized: false },
-    mailbox: "INBOX",
-    searchFilter: ["UNSEEN", ["SINCE", emailDate]],
-    markSeen: true,
-    fetchUnreadOnStart: false,
-    mailParserOptions: {streamAttachments: true},
-    attachments: true,
-    attachmentOptions: { directory: "attachments/" }
-  });
-  mailListener.start();
-  mailListener.on("mail", function(mail, seqno, attributes){
-	console.log('new mail: ', mail);
-});
+// //emial listener
+// var mailListener = new MailListener({
+//     username: configs.username,
+//     password: configs.pass,
+//     host: 'outlook.office365.com',
+//     port: 993,
+//     tls: false,
+//     connTimeout: 100000,
+//     authTimeout: 100000,
+//     debug: console.log,
+//     tlsOptions: { rejectUnauthorized: false },
+//     mailbox: "INBOX",
+//     searchFilter: ["UNSEEN", ["SINCE", emailDate]],
+//     markSeen: true,
+//     fetchUnreadOnStart: false,
+//     mailParserOptions: {streamAttachments: true},
+//     attachments: true,
+//     attachmentOptions: { directory: "attachments/" }
+//   });
+//   mailListener.start();
+//   mailListener.on("mail", function(mail, seqno, attributes){
+// 	console.log('new mail: ', mail);
+// });
 
-mailListener.on("attachment", function(attachment){
-  console.log("*************");
-  console.log(attachment.path);
-});
+// mailListener.on("attachment", function(attachment){
+//   console.log("*************");
+//   console.log(attachment.path);
+// });
 
 const options = {
 key: fs.readFileSync(key),
@@ -87,10 +88,10 @@ const router =  (req, res)=>{
     res.end("hello world\n");
 };
 
-transport().catch("pigs",console.error);
-// app.use(express.json())
-// const httpsServer = require('https').createServer(options,router)
-// .listen(port, ()=> (console.log(new Date() + `\n[HTTPS]: Server is listening on port ${[port]}`)))
+
+ app.use(express.json())
+const httpsServer = require('https').createServer(options,router)
+ .listen(port, ()=> (console.log(new Date() + `\n[HTTPS]: Server is listening on port ${[port]}`)))
 
 
 
--- listen.js
+++ listen.js
@@ -1,33 +1,18 @@
-const MailListener = require('mail-listener2');
+// send sms 
+const springedge = require('springedge');
 
-var emailDate = new Date().getTime();
+let params = {
+  'apikey': '', // API Key 
+  'sender': 'ajalantbrown@gmail.com', // Sender Name 
+  'to': [
+    '3107705607'  //Moblie Number 
+  ],
+  'message': 'test'
+};
 
-var mailListener = new MailListener({
-  username: config.auth.username,
-  password: config.auth.pass,
-  host: config.host,
-  port: 993,
-  tls: true,
-  connTimeout: 10000,
-  authTimeout: 5000,
-  debug: console.log,
-  tlsOptions: { rejectUnauthorized: false },
-  mailbox: "INBOX",
-  searchFilter: ["UNSEEN", ["SINCE", emailDate]],
-  markSeen: true,
-  fetchUnreadOnStart: false,
-  mailParserOptions: {streamAttachments: true},
-  attachments: true,
-  attachmentOptions: { directory: "attachments/" }
-});
-
-mailListener.start();
-
-mailListener.on("mail", function(mail, seqno, attributes){
-	console.log('new mail: ', mail);
-});
-
-mailListener.on("attachment", function(attachment){
-  console.log("*************");
-  console.log(attachment.path);
+springedge.messages.send(params, 5000, function (err, response) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(response);
 });
