abe95af5753e51fb3e1059c78651d140eb2e9955
AJ-Brown-InTech <ajalantbrown@gmail.com>
2022-12-08 18:28:55 -0600

    server

--- index.js
+++ index.js
@@ -15,6 +15,21 @@ require('dotenv').config()
 const key = process.env.KEY
 const cert = process.env.CERT
 
+//https server listener
+const options = {
+key: fs.readFileSync(process.env.KEY),
+cert: fs.readFileSync(process.env.CERT)
+}
+const port = process.env.PORT || 8282
+app.use(express.json())
+const router =  (req, res)=>{
+    res.writeHead(200);
+    res.end("hello world\n");
+};
+const httpsServer = require('https').createServer(options,router)
+.listen(port, ()=> (console.log(new Date() + `\n[HTTP]: Server is listening on port ${[port]}`)))
+
+
 
 
 
